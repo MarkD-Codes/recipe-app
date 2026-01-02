f64bc8c6fbdb3b4edb3f8735bc913ec8829491c0
MarkD-Codes <mark.davidsom@gmail.com>
2026-01-02 23:17:06 +0000

    Added favourites and settings icons to homepage

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {  Router } from '@angular/router';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonButtons, IonIcon } from '@ionic/angular/standalone';
 import { MyData } from '../services/my-data';
 import { RecipesPage } from '../recipes/recipes.page';
 
@@ -9,7 +9,7 @@ import { RecipesPage } from '../recipes/recipes.page';
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, FormsModule, RecipesPage],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonInput, IonButton, IonButtons, IonIcon, FormsModule, RecipesPage],
 })
 export class HomePage {
   Ingredients: string = '';
